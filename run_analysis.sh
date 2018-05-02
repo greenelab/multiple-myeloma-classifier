96ccb3df72565b85b5758d301a0f2f8ea08e114f
gwaygenomics <gregory.way@gmail.com>
2018-05-02 12:39:52 -0400

    add notebook execution

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+execute_time=100000
 # Exit on error
 set -o errexit
 
-# Export to .py files
-jupyter nbconvert --to=python --template=util/rm_magic.tpl --FilesWriter.build_directory=scripts *.ipynb
-
 # Run all files in order
-python scripts/0.get_mad_genes.py
-
+jupyter nbconvert --to=notebook \
+        --ExecutePreprocessor.kernel_name=python3 \
+        --ExecutePreprocessor.timeout=$execute_time \
+        --execute 0.process-data.ipynb
