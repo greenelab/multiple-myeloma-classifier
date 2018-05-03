67fd2c5040fb1bd171673a538645211011e6c21c
gwaygenomics <gregory.way@gmail.com>
2018-05-03 16:36:20 -0400

    add the rest of the notebooks and scripts to pipeline

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-execute_time=100000
 # Exit on error
 set -o errexit
 
+execute_time=10000000
+
 # Run all files in order
+conda activate multiple-myeloma-classifier
+
+# Notebook 1 - Processing Data
 jupyter nbconvert --to=notebook \
         --ExecutePreprocessor.kernel_name=python3 \
         --ExecutePreprocessor.timeout=$execute_time \
         --execute 0.process-data.ipynb
+
+# Notebook 2 - Machine Learning Application
+jupyter nbconvert --to=notebook \
+        --ExecutePreprocessor.kernel_name=python3 \
+        --ExecutePreprocessor.timeout=$execute_time \
+        --execute 1.train-classifier.ipynb
+
+# Notebook 3 - Apply Classifier to Cell Line Data
+jupyter nbconvert --to=notebook \
+        --ExecutePreprocessor.kernel_name=python3 \
+        --ExecutePreprocessor.timeout=$execute_time \
+        --execute 2.apply-classifier.ipynb
+
+# Visualize Coefficients
+Rscript --vanilla visualize_coefficients.R
+
